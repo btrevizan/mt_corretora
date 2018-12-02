fc6e524271249f3bab978580effbdf71c73d2436
GhMalta <gui021096@gmail.com>
2018-12-02 17:41:52 -0200

    Algumas queries implementadas

--- database/queries.sql
+++ database/queries.sql
@@ -9,13 +9,33 @@ JOIN movimentacao as saque ON saque.co_conta_vinculada = conta_vinculada.co_cont
 GROUP BY t.nome;
 
 -- a3. O nome do trader e o total de remuneração por ano (salario + bonus). O salário de um trader é de 150 mil por ano.
-SELECT t.nome
-
+SELECT P.nome Trader, 150000+SUM(valor) Remuneracao
+FROM trader T 
+INNER JOIN Pessoa P USING(cpf)
+NATURAL JOIN rl_traderbonus
+NATURAL JOIN bonus
+GROUP BY P.nome
+ORDER BY Remuneracao DESC;
 
 -- a4. O nome do funcionário de suporte com mais atendimentos.
 
+SELECT P.Nome, COUNT(*) Atendimentos
+FROM Funcionario F
+INNER JOIN Pessoa P USING(cpf)
+INNER JOIN RL_Chat Ch USING(co_funcionario)
+GROUP BY P.Nome, F.co_funcionario
+ORDER BY Atendimentos DESC;
+														   
+														   
 -- b. No mínimo duas delas deve necessitar ser respondida com subconsulta;
 -- b2. O nome do cliente e o valor total em ativos do tipo título prefixado.
+SELECT P.nome, SUM( (OT.quantidade * OT.preco_unitario) ) Valor
+FROM Cliente C 
+INNER JOIN Pessoa P USING(cpf)
+INNER JOIN ordem_titulo OT USING(co_cliente)
+INNER JOIN titulo T USING(co_titulo)
+WHERE T.tipo = 'PreFixado'
+GROUP BY P.cpf, P.nome;
 
 -- c. No mínimo uma delas (diferente da consulta acima) deve necessitar do operador NOT EXISTS para responder questões
 -- do tipo TODOS ou NENHUM que <referencia>
