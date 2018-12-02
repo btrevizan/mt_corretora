ce3baae6db4bb9b0710bb184b0c845aab982ca68
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-12-02 18:20:40 -0200

    New queries

--- database/queries.sql
+++ database/queries.sql
@@ -1,5 +1,5 @@
 -- a. No mínimo uma delas deve necessitar ser respondida com a cláusula group by
--- a2. O nome do trader e o valor médio de seus clientes.
+-- a1. O nome do trader e o valor médio de seus clientes.
 SELECT pessoa.nome, AVG(saldo)
 FROM trader
 JOIN rl_tradercliente as tc ON tc.co_trader = trader.co_trader
@@ -7,7 +7,7 @@ JOIN cliente ON cliente.co_cliente = tc.co_cliente
 JOIN pessoa ON trader.cpf = pessoa.cpf
 GROUP BY pessoa.nome;
 
--- a3. O nome do trader e o total de remuneração por ano (salario + bonus). O salário de um trader é de 150 mil por ano.
+-- a2. O nome do trader e o total de remuneração por ano (salario + bonus). O salário de um trader é de 150 mil por ano.
 SELECT P.nome Trader, 150000+SUM(valor) Remuneracao
 FROM trader T 
 INNER JOIN Pessoa P USING(cpf)
@@ -16,7 +16,7 @@ NATURAL JOIN bonus
 GROUP BY P.nome
 ORDER BY Remuneracao DESC;
 
--- a4. O nome do funcionário de suporte com mais atendimentos.
+-- a3. O nome do funcionário de suporte com mais atendimentos.
 SELECT P.Nome, COUNT(*) Atendimentos
 FROM Funcionario F
 INNER JOIN Pessoa P USING(cpf)
@@ -36,5 +36,10 @@ GROUP BY P.cpf, P.nome;
 
 -- c. No mínimo uma delas (diferente da consulta acima) deve necessitar do operador NOT EXISTS para responder questões
 -- do tipo TODOS ou NENHUM que <referencia>
--- c1. O valor total da conta e nome dos clientes que investem apenas em ações do tipo ON.
+-- c1. O saldo e nome dos clientes que investem apenas em ações do tipo ON.
+SELECT nome, saldo
+FROM cliente NATURAL JOIN pessoa
+WHERE co_cliente IN (SELECT co_cliente FROM ordem_acao NATURAL JOIN acao WHERE acao.nome LIKE '%ON%') AND
+NOT EXISTS (SELECT * FROM ordem_titulo WHERE co_cliente = cliente.co_cliente);
+
 -- c2. O nome dos traders que atendem clientes apenas com perfil agressivo.
